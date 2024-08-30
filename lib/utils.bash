b85438fe3ce2fe925b63c062ccc76de615cc23bb
Herberton Candido Souza <herberton@gmail.com>
2024-08-30 20:00:25 -0300

    fix lint

--- lib/utils.bash
+++ lib/utils.bash
@@ -60,7 +60,6 @@ download_release() {
 	fi
 
 	url="$GH_REPO/releases/download/v${version}/${tool}"
-
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 	
