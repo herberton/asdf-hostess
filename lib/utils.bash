f475032a35a3353e1cfc67efa0952ab816008e31
Herberton Candido Souza <herberton@gmail.com>
2024-08-30 19:43:07 -0300

    fix install release

--- lib/utils.bash
+++ lib/utils.bash
@@ -82,9 +82,7 @@ install_version() {
 		# TODO: Assert hostess executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		
 		chmod +x "$install_path/$tool_cmd"
-
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
