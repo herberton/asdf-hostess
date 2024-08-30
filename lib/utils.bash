d893bb77144bbbc095f615720bab18c54de24bcb
Herberton Candido Souza <herberton@gmail.com>
2024-08-30 19:42:09 -0300

    fix download_release

--- lib/utils.bash
+++ lib/utils.bash
@@ -78,13 +78,13 @@ install_version() {
 	(
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
-		
-		ls -lah
 
 		# TODO: Assert hostess executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		
+		chmod +x "$install_path/$tool_cmd"
+
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
